c06d6d58e7e638bac029eeeb44f79af37d62a859
mgchums <matt.guest@chums.com>
2024-10-22 15:03:49 -0600

    Fixed item colors layout.

--- index.php
+++ index.php
@@ -5,11 +5,12 @@ use chums\user\Groups;
 
 require_once("autoload.inc.php");
 $ui = new WebUI2([
-    'requiredRoles' => Groups::WEB_ADMIN,
+    'requiredRoles' => [Groups::WEB_ADMIN],
     'title' => 'B2B Products Editor',
     'bodyClassName' => 'container-fluid',
     'contentFile' => 'body.inc.php'
 ]);
-$ui->addCSS('https://b2b.chums.com/b2b-swatches/swatches.css')
+$ui->addCSS('/css/b2b-swatches/swatches.css')
+    ->addCSS('./public/css/styles.css')
     ->addManifestJSON('public/js/manifest.json')
     ->render();
