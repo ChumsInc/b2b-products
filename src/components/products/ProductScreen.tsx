9ed16c3ceb98116a23167892e2f2301360c9a3ec
Steve Montgomery <steve@chums.com>
2026-06-09 14:46:26 -0600

    refactor: implement resizable panels in ProductScreen layout
    - Replaced Bootstrap grid with `react-resizable-panels` for improved layout flexibility.
    - Added `useDefaultLayout` for persistent user layout preference management.
    - Integrated `TableResponsiveContainer` for optimized table responsiveness.

--- src/components/products/ProductScreen.tsx
+++ src/components/products/ProductScreen.tsx
@@ -1,24 +1,29 @@
 import ProductTable from "./list/ProductTable";
 import {Outlet} from "react-router";
-import {Col, Row} from "react-bootstrap";
 import ProductTableFilterBar from "./list/ProductTableFilterBar";
 import ProductsLoading from "./list/ProductsLoading";
+import {Group, Panel, Separator, useDefaultLayout} from 'react-resizable-panels';
+import TableResponsiveContainer from "@/components/common/TableResponsiveContainer.tsx";
 
 const ProductScreen = () => {
+    const {onLayoutChanged, defaultLayout} = useDefaultLayout({
+        id: 'b2b-products:product-screen-layout',
+        storage: localStorage,
+    })
     return (
-        <Row className="g-3">
-            <Col md={6} lg={7}>
-                <h2>Product List</h2>
+        <Group defaultLayout={defaultLayout} onLayoutChange={onLayoutChanged}>
+            <Panel defaultSize="50%" className="container" id="product-list-panel">
                 <ProductTableFilterBar/>
-                <ProductsLoading />
-                <div className="table-responsive-xl">
+                <ProductsLoading/>
+                <TableResponsiveContainer>
                     <ProductTable/>
-                </div>
-            </Col>
-            <Col md={6} lg={5}>
+                </TableResponsiveContainer>
+            </Panel>
+            <Separator className="bg-body-secondary rounded-1" style={{minWidth: '10px'}}/>
+            <Panel className="container" defaultSize="50%" id="product-detail-panel">
                 <Outlet/>
-            </Col>
-        </Row>
+            </Panel>
+        </Group>
     )
 }
 
